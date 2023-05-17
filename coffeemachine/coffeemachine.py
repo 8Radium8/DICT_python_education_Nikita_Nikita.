d41cbad1b5ba33970d268450e60c1d9a0b77ca50
Nikita <n.s.yarovyi@student.khai.edu>
2023-05-17 20:19:00 +0300

    stage 2

--- coffeemachine/coffeemachine.py
+++ coffeemachine/coffeemachine.py
@@ -23,3 +23,13 @@ def make_late():
     print("Frothing milk")
     print("Pouring frothed milk into the cup")
     print("Latte is ready!")
+def calculate_ingridients(num_cups):
+    water=num_cups*200
+    milk=num_cups*50
+    coffee_beans=num_cups*15
+    print(f"For {num_cups} cups of coffee U will need:")
+    print(f"{water} ml of water")
+    print(f"{milk} ml of milk")
+    print(f"{coffee_beans} g of coffee beans")
+num_of_cups=int(input("how many cups of coffee U will need?:"))
+calculate_ingridients(num_of_cups)
